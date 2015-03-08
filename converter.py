59daac2e07e103679c730e8ea322a99b676ed29e
Amarto <aar2160@columbia.edu>
2015-03-07 22:07:44 -0500

    Worked on buckets

--- converter.py
+++ converter.py
@@ -1,23 +1,53 @@
 from PIL import Image
-from numpy import *
+
 
 GRADATIONS = 10 # number of color bands to split image into
 
-def get_band(min_color, max_color, grayscale_val):
+'''
+def get_max_min_color(im)
+    color_array = im.getcolors()
+    maximum = color_array[0];
+    minimum = color_array[0];
+    for i in color_array
+        if color_array[i] > maximum
+            maximum = color_array[i]
+        if color_array[i] < minimum
+            minimum = color_array[i]
+
+    return (maximum, minimum)
+'''
+
+
+
+#def get_band(min_color, max_color, grayscale_val):
+def get_band(grayscale_val):
+    
     return grayscale_val / GRADATIONS
 
 # returns 2D grid of numbers from 0 to GRADATIONS specifying
 # which bucket each color goes into
 def get_pixel_grid(img, width, height):
-    img_grid = zeros((width, height)) 
-    """
-    for x in range(1, width):
-        for y in range(height):
-            current_color = get_band(img.getpixel((x, y))
-            img_grid[x, y] = current_color
-    """    
+    img_grid = []
+    
+    for x in range(0, width):
+        current_array = []
+        img_grid.append(current_array)
+        for y in range(0, height):
+            current_color = get_band(img.getpixel((x, y))) #HERE BROKEN
+            print current_color
+            current_array.append(current_color)          
+ # img_grid[x][y] = current_color
+                      
+
     return img_grid
-     
+ 
+def get_buckets(im)
+    (minimum, maximum) = im.getextrema()
+    buckets = []
+    bucket_size = maximum - minimum / GRADATIONS
+    for i in range(GRADATIONS):
+        
+        
 
 def main():
     im = Image.open("example.png")
